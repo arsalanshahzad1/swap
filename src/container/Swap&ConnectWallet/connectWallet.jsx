ad38dbc77e1f1a7883df79d02824b9442ada7914
amigosound <expertmaster30@gmail.com>
2022-10-28 22:29:01 +0500

    nami wallet-integration complete

---
+++ src/container/Swap&ConnectWallet/connectWallet.jsx
@@ -0,0 +1,90 @@
+import React, { useState, useEffect } from 'react'
+
+import { FaTimes } from 'react-icons/fa'
+import { Link, useLocation, useNavigate } from 'react-router-dom'
+import { Main } from '../main'
+import { WALLET } from './constant'
+
+export const ConnectWallet = ({ refreshData, pollWallets, changeAddress }) => {
+    const navigate = useNavigate()
+
+    const wallectConnect = () => {
+        pollWallets()
+        refreshData()
+    }
+
+    useEffect(() => {
+        if (changeAddress !== undefined && changeAddress !== null) {
+            // window.location.replace(`/connect-account/${changeAddress}`)
+            navigate(`/connect-account/${1}`)
+            // navigate(`/connect-account/${changeAddress}`)
+        }
+    }, [changeAddress])
+
+    // let typhon
+    // window.onload = () => {
+    //     typhon = window.cardano.eternl
+
+    //     if (!typhon) {
+    //         alert('Typhon Extension is not installed !')
+    //     }
+    // }
+
+    // async function enable() {
+    //     const isEnabledResponse = await typhon.isEnabled()
+
+    //     if (isEnabledResponse.data === true) {
+    //         // site is already whitlisted, continue using APIs
+    //     } else {
+    //         const enableResponse = await typhon.enable()
+
+    //         if (enableResponse.status === true) {
+    //             // Site whitelisted
+    //         } else {
+    //             // User rejected the whitelisting permission
+    //         }
+    //     }
+    // }
+    const content = (
+        <div className="w-full my-auto sm:w-10/12 md:w-7/12 lg:w-1/2 xl:w-1/3 rounded-2xl z-30 p-4 md:p-6 lg:p-8 2xl:p-10 sm:border-2 sm:border-border-card mx-5 sm:mx-0">
+            <div className="flex items-center justify-end text-white ">
+                <Link to="/">
+                    <FaTimes />
+                </Link>
+            </div>
+
+            <div className="space-y-6">
+                <div className="space-y-2">
+                    <div className="text-white text-2xl font-Semibold">
+                        Connect Wallet
+                    </div>
+                    <p className="text-gray-primary text-sm">
+                        Connect with one of our available wallet providers
+                    </p>
+                </div>
+
+                <div className="border-2 border-border-card rounded-xl overflow-hidden">
+                    {WALLET.map((item, index) => (
+                        <Link
+                            // to={`/connect-account/${changeAddress}`}
+                            to={'#'}
+                            // onClick={(e) => connectWallet()}
+                            onClick={(e) => wallectConnect()}
+                            // address={changeAddress}
+                            key={index}
+                            className="flex items-center space-x-6 text-left text-white bg-gray-secandary px-3 lg:px-5 2xl:px-10 py-4  border border-border-card">
+                            <img
+                                src={item?.img}
+                                alt=""
+                                className="w-1/4 h-10 "
+                            />
+                            <span>{item?.name + ' ' + 'Wallet'}</span>
+                        </Link>
+                    ))}
+                </div>
+            </div>
+        </div>
+    )
+
+    return <Main content={content} />
+}
